dafa5ef081d40274372cba695a300c657ee6707e
TAURUS-ESSEN <reitarovea@gmail.com>
2025-10-23 18:53:01 +0200

    fix: fixed hamburger menu2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,14 +10,14 @@ export default function Header({cart, products}) {
 
     return (
         <>
-        <header className="flex items-center justify-between gap-4">
+        <header className="flex items-center justify-around gap-4 shadow-soft ">
 
-            <div className="min-w-[60px] md:min-w-[80px] py-2  ">
+            <div className="max-w-[60px] md:min-w-[80px] py-2">
                 <Link to='/' className="flex"><img src="/logo.webp" width="100" height="100"/></Link>
             </div>
             <input type="checkbox" id="menu-toggler" />
-            <nav className="w-full "> 
-                <ul className=" flex w-full justify-evenly items-center gap-8 text-base md:text-2xl font-medium text-primary">
+            <nav className="md:w-full"> 
+                <ul className=" flex w-full justify-evenly items-center gap-8 text-lg md:text-2xl font-medium text-primary">
                     <Link to='home' className={location.pathname==='/' || location.pathname==='/home' ? 'active' : ''}>Home</Link>
                     <NavLink to='shop' className={({isActive}) => (isActive? `active` : '')}>Shop</NavLink>
                     <NavLink to='about' className={({isActive}) => (isActive? `active` : '')}>Über uns</NavLink>
@@ -29,14 +29,14 @@ export default function Header({cart, products}) {
                 placeholder="Produkt suchen" 
                 onChange={(e)=>searchProduct(e)} 
                 value={search} 
-                className="search1 border-2 border-secondary rounded-lg p-2 max-w-[180px] md:min-w-[350px]"
+                className="search1 border-2 border-secondary rounded-lg px-4 min-w-[180px] md:min-w-[350px]"
             />
             <label for="menu-toggler" role="button" tabindex="0">
                 <span className="open text-secondary">☰ </span>
                 <span className="close text-secondary border rounded">☰</span>
             </label>
             
-            <div className=" hidden md:flex pl-6 ">
+            <div className=" hidden md:flex pl-6  ">
                 <Link to='cart' className="flex items-center">
                     <i className="fa-solid fa-bag-shopping fa-2xl relative" style={{color: '#7a5230'}}></i>
                     <span className="relative -left-4.5 top-1 text-white w-10">{cart.length}</span>
